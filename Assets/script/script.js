4db6c74f482fcf2b26d8bb11133dca06254bf168
Raul <raulmayas20@gmail.com>
2022-12-12 15:23:57 -0600

    Special Characters and cases

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -16,7 +16,116 @@ function generatePassword() {
       alert("Please enter a value between 8 and 128. Thanks.");
       password = "Please enter a value between 8 and 128. Thanks.";
       return;
-    } 
+    } else {
+      var lowerCase = prompt(
+        "Do you want lowercase letters in your password (Type YES or NO)",
+        "YES"
+      );
+      var upperCase = prompt(
+        "Do you want uppercase letters in your password (Type YES or NO)",
+        "YES"
+      );
+      var numericValues = prompt(
+        "Do you want numeric values in your password (Type YES or NO)",
+        "YES"
+      );
+      var specialSymbols = prompt(
+        "Do you want special characters in your password (Type YES or NO)",
+        "YES"
+      );
+      if (
+        lowerCase === "YES" &&
+        upperCase === "YES" &&
+        numericValues === "YES" &&
+        specialSymbols === "YES"
+      ) {
+        var availableSymbols =
+          "abcdefghijklmnopqrstuvwxyzABCDEFGHIGJKLMNOPQRSTUVWXYZ0123456789#$%&/()?¡¿*¨[]{}-_";
+      } else if (
+        lowerCase === "YES" &&
+        upperCase === "NO" &&
+        numericValues === "NO" &&
+        specialSymbols === "YES"
+      ) {
+        var availableSymbols = "abcdefghijklmnopqrstuvwxyz#$%&/()?¡¿*¨[]{}-_";
+      } else if (
+        lowerCase === "YES" &&
+        upperCase === "YES" &&
+        numericValues === "NO" &&
+        specialSymbols === "NO"
+      ) {
+        var availableSymbols =
+          "abcdefghijklmnopqrstuvwxyzABCDEFGHIGJKLMNOPQRSTUVWXYZ";
+      } else if (
+        lowerCase === "NO" &&
+        upperCase === "NO" &
+        numericValues === "YES" &&
+        specialSymbols === "YES"
+      ) {
+        var availableSymbols = "0123456789#$%&/()?¡¿*¨[]{}-_";
+      } else if (
+        lowerCase === "YES" &&
+        upperCase === "NO" &&
+        numericValues === "YES" &&
+        specialSymbols === "NO"
+      ) {
+        var availableSymbols = "0123456789abcdefghijklmnopqrstuvwxyz";
+      } else if (
+        lowerCase === "NO" &&
+        upperCase === "YES" &&
+        numericValues === "NO" &&
+        specialSymbols === "YES"
+      ) {
+        var availableSymbols = "ABCDEFGHIGJKLMNOPQRSTUVWXYZ#$%&/()?¡¿*¨[]{}-_";
+      } else if (
+        lowerCase === "NO" &&
+        upperCase === "YES" &&
+        numericValues === "YES" &&
+        specialSymbols === "NO"
+      ) {
+        var availableSymbols = "ABCDEFGHIGJKLMNOPQRSTUVWXYZ0123456789";
+      } else if (
+        lowerCase === "NO" &&
+        upperCase === "YES" &&
+        numericValues === "YES" &&
+        specialSymbols === "YES"
+      ) {
+        var availableSymbols =
+          "ABCDEFGHIGJKLMNOPQRSTUVWXYZ0123456789#$%&/()?¡¿*¨[]{}-_";
+      } else if (
+        lowerCase === "YES" &&
+        upperCase === "YES" &&
+        numericValues === "YES" &&
+        specialSymbols === "NO"
+      ) {
+        var availableSymbols =
+          "abcdefghijklmnopqrstuvwxyzABCDEFGHIGJKLMNOPQRSTUVWXYZ0123456789#$%&/()?¡¿*¨[]{}-_";
+      } else if (
+        lowerCase === "YES" &&
+        upperCase === "NO" &&
+        numericValues === "YES" &&
+        specialSymbols === "YES"
+      ) {
+        var availableSymbols =
+          "abcdefghijklmnopqrstuvwxyz0123456789#$%&/()?¡¿*¨[]{}-_";
+      } else if (
+        lowerCase === "YES" &&
+        upperCase === "YES" &&
+        numericValues === "NO" &&
+        specialSymbols === "YES"
+      ) {
+        var availableSymbols =
+          "abcdefghijklmnopqrstuvwxyzABCDEFGHIGJKLMNOPQRSTUVWXYZ#$%&/()?¡¿*¨[]{}-_";
+      } else {
+        alert('Please select two or more character types, to create your strong password.');
+        password = "Please select two or more character types.";
+        return;
+      }
+      for (var i = 0; i < passLength; i++) {
+        var passCharacter = availableSymbols[Math.floor(Math.random() * availableSymbols.length)]
+        password = password + passCharacter
+      }
+    }
   } else {
     alert("Invalid input. Please enter a valid input. Thanks.");
     password = "Invalid input. Please enter a valid input. Thanks.";
